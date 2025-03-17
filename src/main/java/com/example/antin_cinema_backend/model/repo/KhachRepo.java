a31aeea9ccfa7fa439771515e7eb416932fdc6f0
An Nguyen <anndb1903@gmail.com>
2025-03-17 20:52:42 +0700

    khachrepo

---
+++ src/main/java/com/example/antin_cinema_backend/model/repo/KhachRepo.java
@@ -0,0 +1,8 @@
+package com.example.antin_cinema_backend.model.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.antin_cinema_backend.model.entity.Khach;
+
+public interface KhachRepo extends JpaRepository<Khach, Integer> {
+
+}
