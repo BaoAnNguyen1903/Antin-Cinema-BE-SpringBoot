269691c0183e8dad84bde9472a3334b3206d9497
An Nguyen <anndb1903@gmail.com>
2025-03-18 23:20:29 +0700

    UserService

---
+++ src/main/java/com/example/antin_cinema_backend/model/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.antin_cinema_backend.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.antin_cinema_backend.model.entity.User;
+import com.example.antin_cinema_backend.model.repo.UserRepo;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public User getUserById(int uid) {
+        return userRepo.findById(uid).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepo.save(user);
+    }
+
+    public void deleteMovie(int uid) {
+        userRepo.deleteById(uid);
+    }
+}
