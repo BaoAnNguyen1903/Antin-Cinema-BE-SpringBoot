081e2925007995ae05a41f8d665b6b4591be120e
An Nguyen <anndb1903@gmail.com>
2025-03-18 23:46:06 +0700

    MovieShowtimeService

---
+++ src/main/java/com/example/antin_cinema_backend/model/service/MovieShowtimeService.java
@@ -0,0 +1,30 @@
+package com.example.antin_cinema_backend.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.antin_cinema_backend.model.entity.MovieShowtime;
+import com.example.antin_cinema_backend.model.repo.MovieShowtimeRepo;
+
+@Service
+public class MovieShowtimeService {
+    @Autowired
+    private MovieShowtimeRepo movieShowtimeRepo;
+
+    public List<MovieShowtime> getAllMovieShowtimes() {
+        return movieShowtimeRepo.findAll();
+    }
+
+    public MovieShowtime getMovieShowtimeById(int msid) {
+        return movieShowtimeRepo.findById(msid).orElse(null);
+    }
+
+    public MovieShowtime createMovieShowtime(MovieShowtime movieShowtime) {
+        return movieShowtimeRepo.save(movieShowtime);
+    }
+
+    public void deleteMovieShowtime(int msid) {
+        movieShowtimeRepo.deleteById(msid);
+    }
+}
