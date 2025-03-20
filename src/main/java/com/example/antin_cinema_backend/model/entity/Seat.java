d03d48b127ea0b34d8a6073755ea75363cf41783
An Nguyen <anndb1903@gmail.com>
2025-03-20 23:56:15 +0700

    Seat

---
+++ src/main/java/com/example/antin_cinema_backend/model/entity/Seat.java
@@ -0,0 +1,41 @@
+package com.example.antin_cinema_backend.model.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "seat")
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sid;
+
+    @OneToOne
+    @JoinColumn(name = "room", nullable = false)
+    private Room room;
+
+    @Column(length = 16)
+    private String seat_location;
+
+    @OneToOne
+    @JoinColumn(name = "seat_type", nullable = false)
+    private 
+}
---
+++ src/main/java/com/example/antin_cinema_backend/model/entity/SeatType.java
@@ -0,0 +1,5 @@
+package com.example.antin_cinema_backend.model.entity;
+
+public class SeatType {
+
+}
