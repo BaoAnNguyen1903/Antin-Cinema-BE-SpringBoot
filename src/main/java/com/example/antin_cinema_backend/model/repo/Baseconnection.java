545c583359fb306d97b346e8f444916dfd7e2319
An Nguyen <anndb1903@gmail.com>
2025-03-23 19:53:02 +0700

    fix

--- src/main/java/com/example/antin_cinema_backend/model/repo/Baseconnection.java
+++ src/main/java/com/example/antin_cinema_backend/model/repo/Baseconnection.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public class Baseconnection {
     static String url = "jdbc:mysql://localhost:3306/antin_cinema";
     static String username = "root";
