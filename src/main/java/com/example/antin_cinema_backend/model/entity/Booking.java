80b4b90e9198f7093266e2b8c10714aa96aabc07
An Nguyen <anndb1903@gmail.com>
2025-03-24 12:57:05 +0700

    Booking

---
+++ src/main/java/com/example/antin_cinema_backend/model/entity/Booking.java
@@ -0,0 +1,25 @@
+package com.example.antin_cinema_backend.model.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+    private int bid;
+    private User user;
+    private MovieShowtime movieShowtime;
+    private Seat seat;
+    private Discount discount;
+    private LocalDateTime createDate;
+    private float totalPrice;
+    private int status;
+}
