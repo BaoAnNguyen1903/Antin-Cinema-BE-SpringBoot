7796d2386d1f0629f77836bf5439bbb9c4892194
An Nguyen <anndb1903@gmail.com>
2025-03-27 22:17:59 +0700

    MovieShowtimeService

--- src/main/java/com/example/antin_cinema_backend/model/service/MovieShowtimeService.java
+++ src/main/java/com/example/antin_cinema_backend/model/service/MovieShowtimeService.java
@@ -1,30 +1,26 @@
-// package com.example.antin_cinema_backend.model.service;
+package com.example.antin_cinema_backend.model.service;
 
-// import java.util.List;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.antin_cinema_backend.model.entity.MovieShowtime;
+import com.example.antin_cinema_backend.model.repo.MovieShowtimeRepo;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import com.example.antin_cinema_backend.model.entity.MovieShowtime;
-// import com.example.antin_cinema_backend.model.repo.MovieShowtimeRepo;
+@Service
+public class MovieShowtimeService {
+    @Autowired
+    private MovieShowtimeRepo movieShowtimeRepo;
 
-// @Service
-// public class MovieShowtimeService {
-// @Autowired
-// private MovieShowtimeRepo movieShowtimeRepo;
+    public ArrayList<MovieShowtime> getAllMovieShowtimes() throws Exception {
+        return movieShowtimeRepo.getAllMovieShowtimes();
+    }
 
-// public List<MovieShowtime> getAllMovieShowtimes() {
-// return movieShowtimeRepo.findAll();
-// }
+    public MovieShowtime getMovieShowtimeById(int msid) throws Exception {
+        return movieShowtimeRepo.getMovieShowtimeById(msid);
+    }
 
-// public MovieShowtime getMovieShowtimeById(int msid) {
-// return movieShowtimeRepo.findById(msid).orElse(null);
-// }
-
-// public MovieShowtime createMovieShowtime(MovieShowtime movieShowtime) {
-// return movieShowtimeRepo.save(movieShowtime);
-// }
-
-// public void deleteMovieShowtime(int msid) {
-// movieShowtimeRepo.deleteById(msid);
-// }
-// }
+    public MovieShowtime createMovieShowtime(MovieShowtime movieShowtime) throws Exception {
+        movieShowtimeRepo.createMovieShowtime(movieShowtime);
+        return movieShowtime;
+    }
+}
