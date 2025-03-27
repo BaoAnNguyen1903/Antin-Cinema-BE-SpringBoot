675a2e611c26f537baf55f7056d07cee4b3995ec
An Nguyen <anndb1903@gmail.com>
2025-03-27 22:10:39 +0700

    MovieRatedService

--- src/main/java/com/example/antin_cinema_backend/model/service/MovieRatedService.java
+++ src/main/java/com/example/antin_cinema_backend/model/service/MovieRatedService.java
@@ -1,30 +1,26 @@
-// package com.example.antin_cinema_backend.model.service;
+package com.example.antin_cinema_backend.model.service;
 
-// import java.util.List;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.antin_cinema_backend.model.entity.MovieRated;
+import com.example.antin_cinema_backend.model.repo.MovieRatedRepo;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import com.example.antin_cinema_backend.model.entity.MovieRated;
-// import com.example.antin_cinema_backend.model.repo.MovieRatedRepo;
+@Service
+public class MovieRatedService {
+    @Autowired
+    private MovieRatedRepo movieRatedRepo;
 
-// @Service
-// public class MovieRatedService {
-// @Autowired
-// private MovieRatedRepo movieRatedRepo;
+    public ArrayList<MovieRated> getAllMovieRated() throws Exception {
+        return movieRatedRepo.getAllMovieRated();
+    }
 
-// public List<MovieRated> getAllMovieRateds() {
-// return movieRatedRepo.findAll();
-// }
+    public MovieRated getMovieRatedById(int mrid) throws Exception {
+        return movieRatedRepo.getMovieRatedById(mrid);
+    }
 
-// public MovieRated getMovieRatedById(int mrid) {
-// return movieRatedRepo.findById(mrid).orElse(null);
-// }
-
-// public MovieRated createMovieRated(MovieRated movieRated) {
-// return movieRatedRepo.save(movieRated);
-// }
-
-// public void deleteMovieRated(int mrid) {
-// movieRatedRepo.deleteById(mrid);
-// }
-// }
+    public MovieRated createMovieRated(MovieRated movieRated) throws Exception {
+        movieRatedRepo.createMovieRated(movieRated);
+        return movieRated;
+    }
+}
