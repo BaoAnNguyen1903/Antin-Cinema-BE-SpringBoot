2872d2c7442b04a7c884584b3a6b1eb9acba00e3
An Nguyen <anndb1903@gmail.com>
2025-03-27 22:03:15 +0700

    KhachService

--- src/main/java/com/example/antin_cinema_backend/model/service/KhachService.java
+++ src/main/java/com/example/antin_cinema_backend/model/service/KhachService.java
@@ -1,30 +1,26 @@
-// package com.example.antin_cinema_backend.model.service;
+package com.example.antin_cinema_backend.model.service;
 
-// import java.util.List;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.antin_cinema_backend.model.entity.Khach;
+import com.example.antin_cinema_backend.model.repo.KhachRepo;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import com.example.antin_cinema_backend.model.entity.Khach;
-// import com.example.antin_cinema_backend.model.repo.KhachRepo;
+@Service
+public class KhachService {
+    @Autowired
+    private KhachRepo khachRepo;
 
-// @Service
-// public class KhachService {
-// @Autowired
-// private KhachRepo khachRepo;
+    public ArrayList<Khach> getAllKhachs() throws Exception {
+        return khachRepo.getAllKhachs();
+    }
 
-// public List<Khach> getAllKhachs() {
-// return khachRepo.findAll();
-// }
+    public Khach getKhachById(int kid) throws Exception {
+        return khachRepo.getKhachById(kid);
+    }
 
-// public Khach getKhachById(int kid) {
-// return khachRepo.findById(kid).orElse(null);
-// }
-
-// public Khach createKhach(Khach khach) {
-// return khachRepo.save(khach);
-// }
-
-// public void deleteKhach(int kid) {
-// khachRepo.deleteById(kid);
-// }
-// }
+    public Khach createKhach(Khach khach) throws Exception {
+        khachRepo.createKhach(khach);
+        return khach;
+    }
+}
