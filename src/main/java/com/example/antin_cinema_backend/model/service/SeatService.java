c35695a4f6958d506aaca848582a055ffb3a9fcd
An Nguyen <anndb1903@gmail.com>
2025-03-27 22:22:45 +0700

    SeatService

--- src/main/java/com/example/antin_cinema_backend/model/service/SeatService.java
+++ src/main/java/com/example/antin_cinema_backend/model/service/SeatService.java
@@ -1,31 +1,26 @@
-// package com.example.antin_cinema_backend.model.service;
+package com.example.antin_cinema_backend.model.service;
 
-// import java.util.List;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.antin_cinema_backend.model.entity.Seat;
+import com.example.antin_cinema_backend.model.repo.SeatRepo;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+@Service
+public class SeatService {
+    @Autowired
+    private SeatRepo seatRepo;
 
-// import com.example.antin_cinema_backend.model.entity.Seat;
-// import com.example.antin_cinema_backend.model.repo.SeatRepo;
+    public ArrayList<Seat> getAllSeats() throws Exception {
+        return seatRepo.getAllSeats();
+    }
 
-// @Service
-// public class SeatService {
-// @Autowired
-// private SeatRepo seatRepo;
+    public Seat getSeatById(int sid) throws Exception {
+        return seatRepo.getSeatById(sid);
+    }
 
-// public List<Seat> getAllSeats() {
-// return seatRepo.findAll();
-// }
-
-// public Seat getSeatById(int sid) {
-// return seatRepo.findById(sid).orElse(null);
-// }
-
-// public Seat createSeat(Seat seat) {
-// return seatRepo.save(seat);
-// }
-
-// public void deleteSeat(int sid) {
-// seatRepo.deleteById(sid);
-// }
-// }
+    public Seat createSeat(Seat seat) throws Exception {
+        seatRepo.createSeat(seat);
+        return seat;
+    }
+}
