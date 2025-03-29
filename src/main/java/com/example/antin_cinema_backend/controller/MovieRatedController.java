73dfd0ff3c01dfcd6b8191db19aa1443e4128678
An Nguyen <anndb1903@gmail.com>
2025-03-30 00:01:56 +0700

    MovieRatedController

--- src/main/java/com/example/antin_cinema_backend/controller/MovieRatedController.java
+++ src/main/java/com/example/antin_cinema_backend/controller/MovieRatedController.java
@@ -1,41 +1,49 @@
-// package com.example.antin_cinema_backend.controller;
+package com.example.antin_cinema_backend.controller;
 
-// import java.util.List;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.antin_cinema_backend.model.entity.MovieRated;
+import com.example.antin_cinema_backend.model.service.MovieRatedService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import com.example.antin_cinema_backend.model.entity.MovieRated;
-// import com.example.antin_cinema_backend.model.service.MovieRatedService;
+@RestController
+@RequestMapping("/api/v1/movieRated")
+public class MovieRatedController {
+    @Autowired
+    private MovieRatedService movieRatedService;
 
-// @RestController
-// @RequestMapping("/api/v1/movieRated")
-// public class MovieRatedController {
-// @Autowired
-// private MovieRatedService movieRatedService;
+    @GetMapping("/ViewAllMovieRatedList")
+    public ResponseEntity<String> getAllMovieRated() throws Exception {
+        List<MovieRated> movieRateds = movieRatedService.getAllMovieRated();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(movieRateds);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @GetMapping("/ViewAllMovieRatedsList")
-// public List<MovieRated> getAllMovieRateds() {
-// return movieRatedService.getAllMovieRateds();
-// }
+    @GetMapping("/ViewMovieRatedById/{mrid}")
+    public ResponseEntity<String> getUserById(@PathVariable int mrid) throws Exception {
+        MovieRated movieRated = movieRatedService.getMovieRatedById(mrid);
+        if (movieRated == null) {
+            return new ResponseEntity<>("{\"message\": \"Movie Rated not found\"}", HttpStatus.NOT_FOUND);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(movieRated);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @GetMapping("/ViewMovieRatedById/{id}")
-// public MovieRated getMovieRatedById(@PathVariable int id) {
-// return movieRatedService.getMovieRatedById(id);
-// }
-
-// @PostMapping("/CreateNewMovieRated")
-// public MovieRated createMovieRated(@RequestBody MovieRated movieRated) {
-// return movieRatedService.createMovieRated(movieRated);
-// }
-
-// @DeleteMapping("/DeleteMovieRated/{id}")
-// public void deleteMovieRated(@PathVariable int id) {
-// movieRatedService.deleteMovieRated(id);
-// }
-// }
+    @PostMapping("/CreateMovieRated")
+    public ResponseEntity<String> createUser(@RequestBody MovieRated movieRated) throws Exception {
+        MovieRated newMovieRated = movieRatedService.createMovieRated(movieRated);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(newMovieRated);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.CREATED);
+    }
+}
