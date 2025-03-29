016f015c5093ed0355dc174571e22fa111d89585
An Nguyen <anndb1903@gmail.com>
2025-03-30 00:18:47 +0700

    MovieLanguageController

--- src/main/java/com/example/antin_cinema_backend/controller/MovieLanguageController.java
+++ src/main/java/com/example/antin_cinema_backend/controller/MovieLanguageController.java
@@ -1,42 +1,49 @@
-// package com.example.antin_cinema_backend.controller;
+package com.example.antin_cinema_backend.controller;
 
-// import java.util.List;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.antin_cinema_backend.model.entity.MovieLanguage;
+import com.example.antin_cinema_backend.model.service.MovieLanguageService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import com.example.antin_cinema_backend.model.entity.MovieLanguage;
-// import com.example.antin_cinema_backend.model.service.MovieLanguageService;
+@RestController
+@RequestMapping("/api/v1/movieLanguage")
+public class MovieLanguageController {
+    @Autowired
+    private MovieLanguageService movieLanguageService;
 
-// @RestController
-// @RequestMapping("/api/v1/movieLanguage")
-// public class MovieLanguageController {
-// @Autowired
-// private MovieLanguageService movieLanguageService;
+    @GetMapping("/ViewAllMovieLanguagesList")
+    public ResponseEntity<String> getAllMovieLanguages() throws Exception {
+        List<MovieLanguage> movieLanguages = movieLanguageService.getAllMovieLanguages();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(movieLanguages);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @GetMapping("/ViewAllMovieLanguagesList")
-// public List<MovieLanguage> getAllMovieLanguages() {
-// return movieLanguageService.getAllMovieLanguages();
-// }
+    @GetMapping("/ViewMovieLanguageById/{mlid}")
+    public ResponseEntity<String> getMovieLanguageById(@PathVariable int mlid) throws Exception {
+        MovieLanguage movieLanguage = movieLanguageService.getMovieLanguageById(mlid);
+        if (movieLanguage == null) {
+            return new ResponseEntity<>("{\"message\": \"Movie Language not found\"}", HttpStatus.NOT_FOUND);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(movieLanguage);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @GetMapping("/ViewMovieLanguageById/{id}")
-// public MovieLanguage getMovieLanguageById(@PathVariable int id) {
-// return movieLanguageService.getMovieLanguageById(id);
-// }
-
-// @PostMapping("/CreateNewMovieLanguage")
-// public MovieLanguage createMovieLanguage(@RequestBody MovieLanguage
-// movieLanguage) {
-// return movieLanguageService.createMovieLanguage(movieLanguage);
-// }
-
-// @DeleteMapping("/DeleteMovieLanguage/{id}")
-// public void DeleteMovieLanguage(@PathVariable int id) {
-// movieLanguageService.deleteMovieLanguage(id);
-// }
-// }
+    @PostMapping("/CreateMovieLanguage")
+    public ResponseEntity<String> createMovieLanguage(@RequestBody MovieLanguage movieLanguage) throws Exception {
+        MovieLanguage newMovieLanguage = movieLanguageService.createMovieLanguage(movieLanguage);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(newMovieLanguage);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.CREATED);
+    }
+}
