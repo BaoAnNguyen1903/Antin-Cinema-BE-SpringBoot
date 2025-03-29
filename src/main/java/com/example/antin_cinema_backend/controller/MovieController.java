7a545b2c48c6db322b35862803ba0a0e8c295eea
An Nguyen <anndb1903@gmail.com>
2025-03-30 00:29:47 +0700

    MovieController

--- src/main/java/com/example/antin_cinema_backend/controller/MovieController.java
+++ src/main/java/com/example/antin_cinema_backend/controller/MovieController.java
@@ -1,42 +1,49 @@
-// package com.example.antin_cinema_backend.controller;
+package com.example.antin_cinema_backend.controller;
 
-// import java.util.List;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.antin_cinema_backend.model.entity.Movie;
+import com.example.antin_cinema_backend.model.service.MovieService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/v1/movie")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
 
-// import com.example.antin_cinema_backend.model.entity.Movie;
-// import com.example.antin_cinema_backend.model.service.MovieService;
+    @GetMapping("/ViewAllMoviesList")
+    public ResponseEntity<String> getAllMovies() throws Exception {
+        List<Movie> movies = movieService.getAllMovies();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(movies);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @RestController
-// @RequestMapping("/api/v1/movie")
-// public class MovieController {
-// @Autowired
-// private MovieService movieService;
+    @GetMapping("/ViewMovieById/{uid}")
+    public ResponseEntity<String> getMovieById(@PathVariable int mid) throws Exception {
+        Movie movie = movieService.getMovieById(mid);
+        if (movie == null) {
+            return new ResponseEntity<>("{\"message\": \"Movie not found\"}", HttpStatus.NOT_FOUND);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(movie);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @GetMapping("/ViewAllMoviesList")
-// public List<Movie> getAllMovies() {
-// return movieService.getAllMovies();
-// }
-
-// @GetMapping("/ViewMovieById/{id}")
-// public Movie getMovieById(@PathVariable int id) {
-// return movieService.getMovieById(id);
-// }
-
-// @PostMapping("/CreateNewMovie")
-// public Movie createMovie(@RequestBody Movie movie) {
-// return movieService.createMovie(movie);
-// }
-
-// @DeleteMapping("/DeleteMovie/{id}")
-// public void deleteMovie(@PathVariable int id) {
-// movieService.deleteMovie(id);
-// }
-// }
+    @PostMapping("/CreateMovie")
+    public ResponseEntity<String> createUser(@RequestBody Movie movie) throws Exception {
+        Movie newMovie = movieService.createMovie(movie);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(newMovie);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.CREATED);
+    }
+}
