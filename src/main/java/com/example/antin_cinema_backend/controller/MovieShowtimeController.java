e2bf4df607d001218f8e15a94564416e4c1c2b48
An Nguyen <anndb1903@gmail.com>
2025-03-29 23:56:50 +0700

    MovieShowtimeController

--- src/main/java/com/example/antin_cinema_backend/controller/MovieShowtimeController.java
+++ src/main/java/com/example/antin_cinema_backend/controller/MovieShowtimeController.java
@@ -1,42 +1,49 @@
-// package com.example.antin_cinema_backend.controller;
+package com.example.antin_cinema_backend.controller;
 
-// import java.util.List;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.antin_cinema_backend.model.entity.MovieShowtime;
+import com.example.antin_cinema_backend.model.service.MovieShowtimeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import com.example.antin_cinema_backend.model.entity.MovieShowtime;
-// import com.example.antin_cinema_backend.model.service.MovieShowtimeService;
+@RestController
+@RequestMapping("/api/v1/movieShowtime")
+public class MovieShowtimeController {
+    @Autowired
+    private MovieShowtimeService movieShowtimeService;
 
-// @RestController
-// @RequestMapping("/api/v1/movieShowtime")
-// public class MovieShowtimeController {
-// @Autowired
-// private MovieShowtimeService movieShowtimeService;
+    @GetMapping("/ViewAllMovieShowtimesList")
+    public ResponseEntity<String> getAllUsers() throws Exception {
+        List<MovieShowtime> users = movieShowtimeService.getAllMovieShowtimes();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(users);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @GetMapping("/ViewAllMovieShowtimesList")
-// public List<MovieShowtime> getAllMovieShowtimes() {
-// return movieShowtimeService.getAllMovieShowtimes();
-// }
+    @GetMapping("/ViewMovieShowtimeById/{uid}")
+    public ResponseEntity<String> getUserById(@PathVariable int msid) throws Exception {
+        MovieShowtime movieShowtime = movieShowtimeService.getMovieShowtimeById(msid);
+        if (movieShowtime == null) {
+            return new ResponseEntity<>("{\"message\": \"Movie Showtime not found\"}", HttpStatus.NOT_FOUND);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(movieShowtime);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
 
-// @GetMapping("/ViewMovieShowtimeById/{id}")
-// public MovieShowtime getMovieShowtimeById(@PathVariable int id) {
-// return movieShowtimeService.getMovieShowtimeById(id);
-// }
-
-// @PostMapping("/CreateNewMovieShowtime")
-// public MovieShowtime createMovieShowtime(@RequestBody MovieShowtime
-// movieShowtime) {
-// return movieShowtimeService.createMovieShowtime(movieShowtime);
-// }
-
-// @DeleteMapping("/DeleteMovieShowtime/{id}")
-// public void deleteMovieShowtime(@PathVariable int id) {
-// movieShowtimeService.deleteMovieShowtime(id);
-// }
-// }
+    @PostMapping("/CreateMovieShowtime")
+    public ResponseEntity<String> createUser(@RequestBody MovieShowtime movieShowtime) throws Exception {
+        MovieShowtime nMovieShowtime = movieShowtimeService.createMovieShowtime(movieShowtime);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(nMovieShowtime);
+        return new ResponseEntity<>(jsonResponse, HttpStatus.CREATED);
+    }
+}
