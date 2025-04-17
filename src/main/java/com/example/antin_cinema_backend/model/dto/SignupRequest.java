bf0621721e2272413ef1e4256f2bb073d026fa0b
An Nguyen <anndb1903@gmail.com>
2025-04-17 10:44:27 +0700

    registerDTO

--- src/main/java/com/example/antin_cinema_backend/model/dto/SignupRequest.java
+++ src/main/java/com/example/antin_cinema_backend/model/dto/SignupRequest.java
@@ -1,13 +1,21 @@
 package com.example.antin_cinema_backend.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Data
 public class SignupRequest {
     private String name;
-    private String email;
     private String username;
     private String password;
-    private String confirmPassword;
-    private String role;
+    private String email;
 }
