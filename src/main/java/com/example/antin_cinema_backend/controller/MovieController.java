8b9a15976bea0006f2496c62e0e3d81989a57467
An Nguyen <anndb1903@gmail.com>
2025-04-20 13:15:57 +0700

    sai repo

--- src/main/java/com/example/antin_cinema_backend/controller/MovieController.java
+++ src/main/java/com/example/antin_cinema_backend/controller/MovieController.java
@@ -10,10 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.antin_cinema_backend.model.dto.UserUpdateDTO;
 import com.example.antin_cinema_backend.model.entity.Meta;
 import com.example.antin_cinema_backend.model.entity.Movie;
 import com.example.antin_cinema_backend.model.entity.MovieLanguage;
@@ -159,6 +162,22 @@ public class MovieController {
             Map<String, Object> errorResponse = new HashMap<>();
             errorResponse.put("error", "Internal server error");
             return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/UpdateMovie")
+    public ResponseEntity<Map<String, Object>> updateUser(@RequestBody UserUpdateDTO userDTO) throws Exception {
+        boolean updated = movieService.updateUser(userDTO);
+        if (updated) {
+            Map<String, Object> response = Map.of(
+                    "data", true,
+                    "message", "User updated successfully");
+            return ResponseEntity.ok(response);
+        } else {
+            Map<String, Object> response = Map.of(
+                    "data", false,
+                    "message", "User not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
     }
 }
--- src/main/java/com/example/antin_cinema_backend/model/service/MovieService.java
+++ src/main/java/com/example/antin_cinema_backend/model/service/MovieService.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.antin_cinema_backend.model.dto.UserUpdateDTO;
 import com.example.antin_cinema_backend.model.entity.Movie;
+import com.example.antin_cinema_backend.model.entity.User;
 import com.example.antin_cinema_backend.model.repo.MovieRepo;
 
 @Service
@@ -38,5 +41,43 @@ public class MovieService {
     public Movie createMovie(Movie movie) throws Exception {
         movieRepo.createMovie(movie);
         return movie;
+    }
+
+    public boolean updateMovie(Movie dto) throws Exception {
+        Movie existingMovie = movieRepo.getMovieById(dto.getMid());
+        if (existingMovie == null) {
+            return false;
+        }
+
+        // Cập nhật các thuộc tính nếu chúng khác null
+        if (dto.getMovieName() != null)
+            existingMovie.setMovieName(dto.getMovieName());
+        if (dto.getMovieDescription() != null)
+            existingMovie.setMovieDescription(dto.getMovieDescription());
+        if (dto.getMovieDirector() != null)
+            existingMovie.setMovieDirector(dto.getMovieDirector());
+        if (dto.getMovieActor() != null)
+            existingMovie.setMovieActor(dto.getMovieActor());
+        if (dto.getMovieType() != null)
+            existingMovie.setMovieType(dto.getMovieType());
+        if (dto.getMovieTime() != null)
+            existingMovie.setMovieTime(dto.getMovieTime());
+        if (dto.getMovieLanguage() != null)
+            existingMovie.setMovieLanguage(dto.getMovieLanguage());
+        if (dto.getMovieRated() != null)
+            existingMovie.setMovieRated(dto.getMovieRated());
+        if (dto.getPoster() != null)
+            existingMovie.setPoster(dto.getPoster());
+        if (dto.getBanner() != null)
+            existingMovie.setBanner(dto.getBanner());
+        if (dto.getOpenday() != null)
+            existingMovie.setOpenday(dto.getOpenday());
+        if (dto.getCloseday() != null)
+            existingMovie.setCloseday(dto.getCloseday());
+        if (dto.getMovieStatus() != 0)
+            existingMovie.setMovieStatus(dto.getMovieStatus());
+
+        movieRepo.updateMovieById(existingMovie);
+        return true;
     }
 }
