912d53007246f256224ac8a2b019e76a891d6c0f
An Nguyen <anndb1903@gmail.com>
2025-04-21 12:40:42 +0700

    room service

--- src/main/java/com/example/antin_cinema_backend/controller/RoomController.java
+++ src/main/java/com/example/antin_cinema_backend/controller/RoomController.java
@@ -1,6 +1,9 @@
 package com.example.antin_cinema_backend.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.antin_cinema_backend.model.entity.Meta;
+import com.example.antin_cinema_backend.model.entity.PaginateData;
 import com.example.antin_cinema_backend.model.entity.Room;
+import com.example.antin_cinema_backend.model.entity.User;
+import com.example.antin_cinema_backend.model.service.PaginatedResult;
 import com.example.antin_cinema_backend.model.service.RoomService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -20,12 +29,22 @@ public class RoomController {
     @Autowired
     private RoomService romService;
 
-    @GetMapping("/ViewAllRoomsList")
-    public ResponseEntity<String> getAllRooms() throws Exception {
-        List<Room> rooms = romService.getAllRooms();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writeValueAsString(rooms);
-        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    @GetMapping("/ViewAllRooms")
+    public ResponseEntity<Map<String, Object>> getAllUsers(
+            @RequestParam(defaultValue = "1") int current,
+            @RequestParam(defaultValue = "5") int pageSize) throws Exception {
+
+        PaginatedResult<User> paginatedResult = romService.getUsersPaginated(current, pageSize);
+        int total = paginatedResult.getTotal();
+        int pages = (int) Math.ceil((double) total / pageSize);
+
+        Meta meta = new Meta(current, pageSize, pages, total);
+        PaginateData<User> paginatedData = new PaginateData<>(meta, paginatedResult.getResult());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", paginatedData);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/ViewRoomById/{rid}")
